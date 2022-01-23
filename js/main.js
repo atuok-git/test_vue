ce83176a0c017a822bf69e30e5b0dcc9e9d28bb0
kota fukumoto <kota@mbp.local>
2022-01-23 17:53:56 +0900

    フォーム入力バインディング

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 const app = Vue.createApp({
     data: () => ({
-        newItem: '',
-        todos: [],
-
+        message: 'hello',
+        checked: false,
+        colors: [],
+        selected: '',
     }),
     methods: {
-        addItem: function(event) {
-            let todo = {
-                item: this.newItem,
-                isDone: false,
-            }
-            if(this.newItem === '') return
-            this.todos.push(todo)
-            this.newItem = ''
-        },
-        deleteItem: function(index) {
-            this.todos.splice(index, 1)
-        }
-    } 
+    }
 })
 app.mount('#app')
