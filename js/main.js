710d367719f5dbe1471d61930a95f1b4e1a72277
kota fukumoto <kota@mbp.local>
2022-01-23 14:44:31 +0900

    監視プロパティの基本

--- js/main.js
+++ js/main.js
@@ -1,9 +1,52 @@
 const app = Vue.createApp({
     data: () => ({
-        newItem: '',
-        todos: [],
-
+        message: 'Hello Vue.js!!!',
+        km: 0,
+        m: 0,
+        firstName: '',
+        lastName: '',
+        // fullName: '',
+        colors: [
+            { name: 'Red'},
+            { name: 'Green'},
+            { name: 'Blue'},
+        ],
     }),
+    watch: {
+        message: function(newValue, oldValue) {
+            console.log('new: %s, old: %s', newValue, oldValue)
+        },
+        km: function(value) {
+            console.log(value)
+            this.km = value
+            this.m = value * 1000
+        },
+        m: function(value) {
+            this.km = value / 1000
+            this.m = value
+        },
+
+        // firstName: function(value) {
+        //     this.fullName = value + ' ' + this.lastName
+        // },
+        // lastName: function(value) {
+        //     this.fullName = this.firstName + ' ' + value 
+        // },
+
+        colors: {
+            handler: function(newValue, oldValue) {
+                console.log('Update!!!')
+            },
+            deep: true
+        }
+
+    },
+    computed: {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
+        }
+
+    },
     methods: {
         addItem: function(event) {
             let todo = {
@@ -16,6 +59,9 @@ const app = Vue.createApp({
         },
         deleteItem: function(index) {
             this.todos.splice(index, 1)
+        },
+        onClick: function(event) {
+            this.colors[1].name = 'White'
         }
     } 
 })
