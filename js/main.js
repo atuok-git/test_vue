bc4b8186019651cc9bed8ba9ff2df2775a0f1ae4
kota fukumoto <kota@mbp.local>
2022-01-23 16:02:18 +0900

    QiitaAPIを用いたキーワード検索

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
 const app = Vue.createApp({
     data: () => ({
-        newItem: '',
-        todos: [],
-
+        items: null,
+        keyword: '',
+        message: '',
     }),
+    watch: {
+        keyword: function(newKeyword, oldKeyword) {
+            console.log(newKeyword)
+            this.message = 'Wating for you to stop typing...'
+            this.debouncedGetAnswer()
+        }
+    },
+    mounted: function() {
+        // this.keyword = 'Javascript'
+        // this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
     methods: {
-        addItem: function(event) {
-            let todo = {
-                item: this.newItem,
-                isDone: false,
+        getAnswer: function() {
+            if(this.keyword === '') {
+                console.log('karamoji')
+                this.items = null
+                return
             }
-            if(this.newItem === '') return
-            this.todos.push(todo)
-            this.newItem = ''
-        },
-        deleteItem: function(index) {
-            this.todos.splice(index, 1)
+
+            this.message = 'Loading...'
+            const vm = this
+            console.log(vm)
+            const params = { page: 1, per_page: 20, query: this.keyword}
+            axios.get('https://qiita.com/api/v2/items', { params })
+                .then(function(response) {
+                    console.log(response)
+                    vm.items = response.data
+                })
+                .catch(function(error) {
+                    vm.message ='Error!' + error
+                })
+                .finally(function() {
+                    vm.message = ''
+                })
         }
     } 
 })
